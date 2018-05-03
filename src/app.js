e4c0fda723b9f6d3bba8d0a13546101cf3c36f6b
Usman Balesaria <ubalesaria@me.com>
2018-05-03 16:34:42 +0100

    Adding initial files

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
+// import angular from 'angular';
 
+// Styles in use
+import './scss/style.scss';
+import 'bulma';
+
+// Our modules
+// import Router from './config/router';
+// import Auth from './config/auth';
