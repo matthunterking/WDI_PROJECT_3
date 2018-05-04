93b445bf2a6e5e5515f19b9ce983215b56621c7f
Stephanie Ye <stephanie.ye@gmail.com>
2018-05-04 10:53:11 +0100

    created user model

--- config/router.js
+++ config/router.js
@@ -13,7 +13,9 @@ router.route('/jobs/:id')
   .delete(jobs.delete);      /* <-- secureRoute will be added here */
 
 router.post('/register', auth.register);
-router.post('/login', auth.login);  /* <-- Not Needed as we are using the home screen as the login page */
+router.post('/login', auth.login);
+
+/* <-- Not Needed as we are using the home screen as the login page */
 
 // router.post('/github', oauth.github);  /* <-- Use when needed with OAuth  */
 // router.post('/facebook', oauth.facebook);  /* <-- Use when needed with OAuth  */
