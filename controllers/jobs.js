b8ce5dc8146e1389f29399a9210c82a0da68a369
matthunterking <matthew85king@gmail.com>
2018-05-04 09:59:20 +0100

    Added jobs create, update, index and show

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,3 +1,41 @@
 const Job = require('../models/job');
 
-function jobsIndex();
+function jobsIndex(req, res, next) {
+  Job
+    .find()
+    .exec()
+    .then(jobs => res.json(jobs))
+    .catch(next);
+}
+
+function jobsShow(req, res, next) {
+  Job
+    .findById(req.params.id)
+    .populate('jobs.createdBy')
+    .exec()
+    .then(job => {
+      if(!job) return res.sendStatus(404);
+      res.json(job);
+    })
+    .catch(next);
+}
+
+function jobsCreate(req, res, next) {
+  Job
+    .create(req.body)
+    .then(job => res.status(201).json(job))
+    .catch(next);
+}
+
+function jobsUpdate(req, res, next) {
+  Job
+    .findById(req.params.id)
+    .exec()
+    .then(job => {
+      if(!job) return res.sendStatus(404);
+      return Object.assign(job, req.body);
+    })
+    .then(job => job.save())
+    .then(job => res.json(job))
+    .catch(next);
+}
