6472a28162cfded84b3d9ff857a37a72f1864378
Usman Balesaria <ubalesaria@me.com>
2018-05-04 15:29:14 +0100

    Error handling functionality has been added

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ const bodyParser = require('body-parser');
 
 const router = require('./config/router');
 const { port, dbURI } = require('./config/environment');
+const errorHandler = require('./lib/errorHandler');
 
 mongoose.connect(dbURI);
 
 app.use(bodyParser.json());
 app.use('/api', router);
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`We're up and running on port ${port}`));
