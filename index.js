7d4d397bd70857cd558c3346daaa3920dfb4e896
Usman Balesaria <ubalesaria@me.com>
2018-05-04 10:19:54 +0100

    Adding routes to router file

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const router = require('./config/router');
 
 const { port } = require('./config/environment');
 // const router = require('./config/router');
+
+app.use('/api', router);
 
 
 app.listen(port, () => console.log(`We're up and running on port ${port}`));
