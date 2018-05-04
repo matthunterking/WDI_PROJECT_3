66792a8c7682c0fdd7506c9a257ac54015cecc98
Stephanie Ye <stephanie.ye@gmail.com>
2018-05-04 10:11:12 +0100

    created jobSchema

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 
-const { port } = require('./config/environment');
-// const router = require('./config/router');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
+const { port, dbURI } = require('./config/environment');
+// const router = require('./config/router');
+mongoose.connect(dbURI);
+
+app.use(bodyParser.json());
 
 app.listen(port, () => console.log(`We're up and running on port ${port}`));
