5a4add36dcd51245777a7ada3930b0b5f176ae96
Stephanie Ye <stephanie.ye@gmail.com>
2018-05-05 10:06:51 +0100

    completed routing with view for user index

--- src/config/router.js
+++ src/config/router.js
@@ -1,3 +1,11 @@
+secureState.$inject = ['$q', '$auth', '$state'];
+function secureState($q, $auth, $state) {
+  return new $q(resolve => {
+    if($auth.isAuthenticated()) return resolve();
+    $state.go('login');
+  });
+}
+
 Router.$inject = ['$stateProvider', '$urlRouterProvider'];
 
 function Router($stateProvider, $urlRouterProvider) {
@@ -5,6 +13,26 @@ function Router($stateProvider, $urlRouterProvider) {
     .state('home', {
       url: '/',
       templateUrl: 'views/home.html'
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'views/auth/login.html',
+      controller: 'LoginCtrl as login'
+    })
+    .state('register', {
+      url: '/register',
+      templateUrl: 'views/auth/register.html',
+      controller: 'RegisterCtrl as register'
+    })
+    .state('usersIndex', {
+      url: '/users',
+      templateUrl: 'views/users/index.html',
+      controller: 'UsersIndexCtrl as usersIndex'
+    })
+    .state('usersShow', {
+      url: '/users/:id',
+      templateUrl: 'views/users/show.html',
+      controller: 'UsersShowCtrl as usersShow'
     });
 
   $urlRouterProvider.otherwise('/');
