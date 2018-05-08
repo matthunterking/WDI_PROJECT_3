e5dc60a65084cd7dc8d55c2ea717d5a916f4bc65
Usman Balesaria <ubalesaria@me.com>
2018-05-08 17:27:49 +0100

    Sign in with google pop up is now working. Needs further tweaking to be fully functional

--- src/config/auth.js
+++ src/config/auth.js
@@ -5,10 +5,20 @@ function Auth($authProvider) {
   $authProvider.signupUrl = '/api/register';
 
   $authProvider.google({
-    clientId: '188667229153-j0d8s2ul5lls4l3tggse2lbputgaav1t.apps.googleusercontent.com',
-    url: 'https://accounts.google.com/o/oauth2/auth?'
+    url: '/auth/google',
+    clientId: '188667229153-shoma2uvkvfojmif31r2fb28u9qreglb.apps.googleusercontent.com',
+    authorizationEndpoint: 'https://accounts.google.com/o/oauth2/auth',
+    redirectUri: window.location.origin,
+    requiredUrlParams: ['scope'],
+    optionalUrlParams: ['display'],
+    scope: ['profile', 'email'],
+    scopePrefix: 'openid',
+    scopeDelimiter: ' ',
+    display: 'popup',
+    oauthType: '2.0',
+    popupOptions: { width: 452, height: 633 }
   });
 
-}
+} 
 
 export default Auth;
