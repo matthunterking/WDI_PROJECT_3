13dd06413c8f5825fd693bae3bd13ac55f250aa0
Usman Balesaria <ubalesaria@me.com>
2018-05-11 10:37:35 +0100

    Fixed bug with db uri on heroku

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,6 @@
 const port = process.env.PORT || 4000;
 const env = process.env.NODE_ENV || 'dev';
-const dbURI = process.env.MONGOD_URI || `mongodb://localhost/neighbourgood-${env}`;
+const dbURI = process.env.MONGODB_URI || `mongodb://localhost/neighbourgood-${env}`;
 const secret = process.env.SECRET || 'G6^sk*/>ersTSauV2';
 
 module.exports = { port, dbURI, secret, env };
